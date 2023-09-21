e8621454378230230c4b75fff41d05cfb23c86af
LuisAlfredo-Campos <alfredocampos.s@icloud.com>
2023-09-21 18:24:03 -0300

    comit inicial

---
+++ python_work/full_name3.py
@@ -0,0 +1,9 @@
+first_name ="ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message) 
+print("\t python")
+print("Languages : \npython \nC \nJavaScript ")
+favorite_language = ' python '
+favorite_language.rstrip()
---
+++ python_work/name.py
@@ -0,0 +1,8 @@
+message = "¡hello python world!"
+print(message)
+
+mesage = "¡hello python crash course world!"
+print(mesage)
+
+name = "ada lovelace"
+print(name.title()) 
