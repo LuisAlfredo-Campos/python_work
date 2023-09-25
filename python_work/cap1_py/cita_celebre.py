02099f894d4b85cc8e1ef477c116c392e888134e
LuisAlfredo-Campos <alfredocampos.s@icloud.com>
2023-09-25 20:10:29 -0300

    cambios nuevos

---
+++ python_work/cap1_py/cita_celebre.py
@@ -0,0 +1,6 @@
+cita_celebre = 'C.S Lewis once said , “Creo en el cristianismo como creo que el Sol ha salido. No sólo porque lo vea sino porque gracias a eso puedo ver todo lo demás.” '
+print(cita_celebre)
+famous_person = "C.S Lewis"
+message = "Creo en el cristianismo como creo que el Sol ha salido. No sólo porque lo vea sino porque gracias a eso puedo ver todo lo demás."
+citacelebre = f"{famous_person} {message}" # con f puedo agrrgar e incorpar dos lineas de codigo o variables y unirlas para una sola  salida 
+print(citacelebre)
---
+++ python_work/cap1_py/numeros.py
@@ -0,0 +1,6 @@
+print(5+3) # 
+print(16/2)
+print(4*2)
+print(10-2)
+number_favorito = 9
+print(number_favorito)
